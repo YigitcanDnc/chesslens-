c36e66333060f3b860e00f54fc83d16c241ec6e6
Yiğitcan <40703768+YigitcanDnc@users.noreply.github.com>
2026-05-11 14:07:55 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function initStockfish() {
   document.getElementById('status-text').textContent = 'Başlatılıyor…';
 
   // Kendi projemizdeki dosyayı çağırıyoruz. Sıfır CORS hatası, tam performans.
-  const worker = new Worker('stockfish.js');
+  const worker = new Worker('worker.js');
 
   worker.onmessage = (e) => {
     handleSFMessage(e.data);
